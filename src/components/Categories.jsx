4728a1d8fdc978d4e8045fa3a27f8bbd476fac3c
Imtiaz Khandoker <imtiazkhandoker@Imtiazs-MacBook.local>
2023-08-28 23:50:51 +0600

    Enable cors

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,33 +1,34 @@
-import { useState, useEffect } from 'react'
-import axios from "axios"
-import SectionTitle from "~/components/ui/SectionTitle"
-import CategoryItem from "~/components/ui/CategoryItem"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import SectionTitle from "~/components/ui/SectionTitle";
+import CategoryItem from "~/components/ui/CategoryItem";
 
 function Categories() {
-
-  const [categoryList, setcategoryList] = useState([])
+  const [categoryList, setcategoryList] = useState([]);
 
   useEffect(() => {
-    axios.get('/fake_data/categories.json').then((res) => {
-      setcategoryList(res.data)
-    })
-  }, [])
+    axios.get("http://localhost:3000/category").then((res) => {
+      setcategoryList(res.data);
+    });
+  }, []);
+
+  console.log("category", categoryList);
 
   return (
     <div>
       <SectionTitle>Categories</SectionTitle>
-      {categoryList.map((object, index) =>
+      {categoryList.map((object, index) => (
         <CategoryItem
           key={index}
           index={index}
           url={object.url}
-          title={object.title}
+          title={object.name}
           img={object.img}
           posts={object.posts}
         />
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
--- src/components/ui/SinglePostItem.jsx
+++ src/components/ui/SinglePostItem.jsx
@@ -1,18 +1,21 @@
 function SinglePostItem({ url, img, category, title, date }) {
+  console.log("img", img);
   return (
-    <a href={url} className="p-4 rounded-xl border border-solid border-[#E8E8EA]">
-      <img src={img} className="h-52 w-full rounded-md object-cover mb-4" />
+    <a
+      href={url}
+      className="p-4 rounded-xl border border-solid border-[#E8E8EA]"
+    >
+      <img
+        src={`http://localhost:3000/` + img}
+        className="h-52 w-full rounded-md object-cover mb-4"
+      />
       <span className="inline-block px-2 py-1 bg-[#4B6BFB] bg-opacity-5 text-[#4B6BFB] text-sm rounded-md">
         {category}
       </span>
-      <h2 className="font-medium text-xl mt-2">
-        {title}
-      </h2>
-      <span className="inline-block mt-2 text-[#97989F] text-sm">
-        {date}
-      </span>
+      <h2 className="font-medium text-xl mt-2">{title}</h2>
+      <span className="inline-block mt-2 text-[#97989F] text-sm">{date}</span>
     </a>
-  )
+  );
 }
 
-export default SinglePostItem
+export default SinglePostItem;
